a814cc76b0602c44566ac3403d91cab6cb221450
vebzzdude <udaynamburu@gmail.com>
2019-03-05 17:11:53 +0530

    Update reverse_each_word_in_a_sentence.c

--- strings/reverse_each_word_in_a_sentence.c
+++ strings/reverse_each_word_in_a_sentence.c
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 #include <ctype.h>
-#include<math.h>
+#include <math.h>
 
-void string_reverse(char *a)
+void words_reversed_in_string(char *a)
 {
   int i = 0, j = 0,  k = 0, len = 0;
 
@@ -15,55 +15,30 @@ void string_reverse(char *a)
 
   for (; i < len; i++)
     {
+      /* Find end of the current word. If it's the last word
+       * in the sentence, it contains End-Of-Line character.
+       */
       for (; (a[j] != ' ' && a[j] != '\0'); j++);
-      k = j;
+      k = j; /* Preserve the index value of space found after end of the word */
       j--;
+      printf("Length of word to be reversed: %d\n", (j-i+1));
+     
+      /* Reverse the word */
       for ( ; i < j; i++, j--)
         {
           a[i] = a[i] ^ a[j];
           a[j] = a[i] ^ a[j];
           a[i] = a[i] ^ a[j];
         }
+     
       printf("String after iteration %d: %s\n", i, a);
+      /* Adjust i and j to process next word in the string */
       i = k;
       j = k+1;
-      printf("a[%d] = %c, a[%d] = %c\n", i, a[i], j, a[j]);
     }
-   
+  
   a[len] = '\0';
-
 }
-
-#if 0
-void string_reverse2(char *a)
-{
-  int i = 0, j = 0,  k = 0, len = 0, sub_len = 0;
-
-  // Calculate string length
-  for (len = 0; a[len] != '\0'; len++);
-  printf("String length: %d\n", len);
-
-  for (i = 0; i < len; i++)
-    {
-      k = i;
-      for (sub_len = 0; (a[i] != ' ' && a[i] != '\0'); i++, sub_len++);
-      printf("sub_len = %d\n", sub_len);
-      for (j = k + sub_len -1; k < j; k++, j--)
-        {
-          a[k] = a[k] ^ a[j];
-          a[j] = a[k] ^ a[j];
-          a[k] = a[k] ^ a[j];
-        }
-      printf("String after iteration %d: %s\n", i, a);
-      i =  i+sub_len+1;
-      j = i;
-      printf("a[%d] = %c, a[%d] = %c\n", i, a[i], j, a[j]);
-    }
-
-  a[len] = '\0';
-
-}
-#endif
 
 int main()
 {
@@ -71,8 +46,8 @@ int main()
   printf("\nEnter a string of less than 100 characters:");
   scanf("%100[^\n]", str);
   printf("Original String: %s\n", str);
-  string_reverse(str);
-  printf("Reversed String: %s\n", str);
+  words_reversed_in_string(str);
+  printf("String after each word is reversed: %s\n", str);
 
   return 0;
 }
